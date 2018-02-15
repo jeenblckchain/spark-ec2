25959972dcd981f88c364de006533e6d954f9f17
jeenblckchain <jeensjunkmail@gmail.com>
2018-02-15 16:17:37 +1100

    Update init.sh

--- persistent-hdfs/init.sh
+++ persistent-hdfs/init.sh
@@ -33,7 +33,7 @@ case "$HADOOP_MAJOR_VERSION" in
     echo "Unpacking Hadoop"
     tar xvzf hadoop-*.tar.gz > /tmp/spark-ec2_hadoop.log
     rm hadoop-*.tar.gz
-    mv hadoop-2.4.0/ persistent-hdfs/
+    mv hadoop-2.7.0/ persistent-hdfs/
 
     # Have single conf dir
     rm -rf /root/persistent-hdfs/etc/hadoop/
